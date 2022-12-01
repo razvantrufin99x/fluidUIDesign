f6e65dbc0c860c013876cc40af19f745d53b428b
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:24:30 +0200

    Add project files.

---
+++ fluidUIDesign/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace fluidUIDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(-3, -14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 16);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 481);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+
+
+    }
+}
+
---
+++ fluidUIDesign/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace fluidUIDesign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Graphics g;
+        public Pen pen0 = new Pen(Color.Black, 1);
+        public Pen pen2 = new Pen(Color.Red, 1);
+        public Brush brush0 = new SolidBrush(Color.Red);
+        public int cx = 400;
+        public int cy = 10;
+        public int ismd = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            drawall();
+        }
+
+        public void drawall()
+        {
+            g.Clear(BackColor);
+            try
+            {
+
+                g.DrawArc(pen0, 5 + cx, 0, cx, cy, 135, 120);
+                g.DrawArc(pen2, cx - cx - 5, 0, cx, cy, 285, 120);
+                //g.DrawRectangle(pen0, 5 + cx, 0, cx, cy);
+                //g.DrawRectangle(pen2, cx - cx - 5, 0, cx, cy);
+                g.DrawEllipse(pen0, 0 + cx - (cx / 5) - 12, cy - (cy / 5), cx / 2, cy / 2);
+                g.FillEllipse(brush0, 0 + cx - (cx / 5) - 12, cy - (cy / 5), cx / 2, cy / 2);
+                //g.DrawRectangle(pen0, cx - (cx / 5) - 10, cy - (cy / 5),cx / 2, cy / 2);
+            }
+            catch { }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cx = e.X;
+            cy = e.Y;
+            if (ismd == 1)
+            {
+                drawall();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ismd = 0;
+            if (ismd == 0)
+            {
+                while (cy > 30)
+                {
+                    cy -= 20;
+                    drawall();
+                    Thread.Sleep(20);
+                }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ismd = 1;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            drawall();
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            ismd = 1;
+            drawall();
+        }
+    }
+}
